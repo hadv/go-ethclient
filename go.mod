517759beee40b6b34a793f174622be828920afc7
hadv <dvietha@gmail.com>
2019-07-18 18:03:12 +0700

    use go mod

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/hadv/go-ethclient
+
+go 1.12
+
+require (
+	github.com/allegro/bigcache v1.2.1 // indirect
+	github.com/aristanetworks/goarista v0.0.0-20190712234253-ed1100a1c015 // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/elastic/gosigar v0.10.4 // indirect
+	github.com/ethereum/go-ethereum v1.9.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/rs/cors v1.6.0 // indirect
+	github.com/steakknife/bloomfilter v0.0.0-20180922174646-6819c0d2a570 // indirect
+	github.com/steakknife/hamming v0.0.0-20180906055917-c99c65617cd3 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+)
--- main.go
+++ main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"math/big"
 
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/clique"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	client, err := ethclient.Dial("https://mainnet.infura.io/v3/b3a5d86cf50440a381fd144ee0dc0450")
+	client, err := ethclient.Dial("https://rinkeby.infura.io/v3/6e8d7fef09c9485daac48699bea64f66")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	latest, err := client.BlockByNumber(context.Background(), nil)
+	block, err := client.BlockByNumber(context.Background(), big.NewInt(4753195))
 	if err != nil {
 		log.Fatal(err)
 	}
+	pubkey, err := crypto.Ecrecover(clique.SealHash(block.Header()).Bytes(), block.Extra()[len(block.Extra())-65:])
+	if err != nil {
+		log.Println(err)
+	}
+	var signer common.Address
+	copy(signer[:], crypto.Keccak256(pubkey[1:])[12:])
+	log.Println(signer.Hex())
 	// fmt.Printf("blk num: %v", latest.Number())
 	// fmt.Println()
 	// bingo := big.NewInt(7158774)
@@ -38,30 +47,30 @@ func main() {
 	// fmt.Println()
 	// fmt.Printf("%v blocks ago", latest.NumberU64()-bingo.Uint64())
 
-	for _, tx := range latest.Transactions() {
-		fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-		fmt.Println(tx.Value().String())    // 10000000000000000
-		fmt.Println(tx.Gas())               // 105000
-		fmt.Println(tx.GasPrice().Uint64()) // 102000000000
-		fmt.Println(tx.Nonce())             // 110644
-		fmt.Println(tx.Data())              // []
-		fmt.Println(tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
+	// for _, tx := range latest.Transactions() {
+	// 	fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+	// 	fmt.Println(tx.Value().String())    // 10000000000000000
+	// 	fmt.Println(tx.Gas())               // 105000
+	// 	fmt.Println(tx.GasPrice().Uint64()) // 102000000000
+	// 	fmt.Println(tx.Nonce())             // 110644
+	// 	fmt.Println(tx.Data())              // []
+	// 	fmt.Println(tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
 
-		signer := types.NewEIP155Signer(tx.ChainId())
-		sender, err := signer.Sender(tx)
-		if err == nil {
-			fmt.Printf("sender: %v", sender.Hex())
-			fmt.Println()
-		} else {
-			fmt.Println(err)
-		}
-		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
-		if err != nil {
-			log.Fatal(err)
-		}
+	// 	signer := types.NewEIP155Signer(tx.ChainId())
+	// 	sender, err := signer.Sender(tx)
+	// 	if err == nil {
+	// 		fmt.Printf("sender: %v", sender.Hex())
+	// 		fmt.Println()
+	// 	} else {
+	// 		fmt.Println(err)
+	// 	}
+	// 	receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-		fmt.Println(receipt.Status) // 1
-	}
+	// 	fmt.Println(receipt.Status) // 1
+	// }
 
 	// blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
 	// count, err := client.TransactionCount(context.Background(), blockHash)
